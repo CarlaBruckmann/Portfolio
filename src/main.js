0321a82ec5ca0242f9df5bfb442a7dbfa3e12db2
Carla Bruckmann <carlaabruckmann@gmail.com>
2024-02-14 22:48:16 -0300

    (feat(app) added responsive menu

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@
 })();
 
 var menuUL = document.getElementsByTagName("ul");
-var heightOful = menuUL[0].scrollHeight;
 var btnToggle = document.getElementsByClassName("btn-toggle");
 btnToggle[0].addEventListener("click", function () {
   if (menuUL[0].style.maxHeight) {
